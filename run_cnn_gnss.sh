83390239706940bb295da66667ea539a71278f3e
dsrim <rim@wustl.edu>
2022-04-20 13:39:05 -0500

    remove unused commands; fix fname error

--- run_cnn_gnss.sh
+++ run_cnn_gnss.sh
@@ -8,7 +8,4 @@ echo "Processing data" \
 && bsub -K < scripts/cnn_gnss_test.bsub \
 && echo "plotting prediction results" \
 && bsub -K < scripts/cnn_gnss_pred_plot.bsub \
-&& echo "transfering data" \
-&& export LSF_DOCKER_ENTRYPOINT=/bin/sh \
-&& export LSF_DOCKER_VOLUMES='/home/rim:/home/rim' \
-&& bsub -K < scripts/cnn_gnss_transfer.bsub
+
--- scripts/cnn_gnss_train.py
+++ scripts/cnn_gnss_train.py
@@ -2,7 +2,7 @@
 import cnn_gnss
 
 nstations = 40  
-fname = '_output/picked_stations_n{:04d}'.format(nstations)
+fname = '_output/picked_stations_n{:04d}.txt'.format(nstations)
 with open(fname, mode='r') as infile:
     gnss_stations_partial_list = infile.readlines()
 
@@ -10,7 +10,7 @@ gnss_stations_partial_list =\
   [s.rstrip('\n') for s in gnss_stations_partial_list]
 
 
-A = cnn_gnss.GNSS_gauge_model()
+A = cnn_gnss.GNSS_gauge_model(train_device='cuda')
 A.load_data()
 A.train_ensemble(nensemble=25, 
                  nepochs=800,
